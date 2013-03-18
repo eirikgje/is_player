8b074243a1cc5b9fe9b34577f262d4c8a9d7784e
Eirik Gjerloew <egjerlow@gmail.com>
2013-03-18 21:32:24 +0000

    Added rudimentary signal handling, so it properly closes the sound device when SIGINT or SIGTERM is sent.

--- is_player.c
+++ is_player.c
@@ -4,14 +4,37 @@
 #include <math.h>
 #include <fcntl.h>
 #include <string.h>
+#include <signal.h>
+#include <stdlib.h>
 #include "is_player_utils.h"
+
+int fp;
+
+void sig_handler(int signo)
+{
+    if (signo == SIGINT || signo == SIGTERM)
+    {
+        ioctl(fp, KIOCSOUND, 0);
+        close(fp);
+        exit(0);
+    }
+}
 
 int main()
 {
+    struct sigaction act;
+    memset(&act, '\0', sizeof(act));
+    act.sa_handler = &sig_handler;
+//    act.sa_sigaction = &sig_handler;
+//    act.sa_flags = SA_SIGINFO;
+    sigemptyset(&act.sa_mask);
+    sigaddset(&act.sa_mask, SIGINT);
+    sigaddset(&act.sa_mask, SIGTERM);
+    sigaction(SIGTERM, &act, NULL);
+    sigaction(SIGINT, &act, NULL);
     init_arrs();
     // 2 seconds is the length of a whole note now
     set_ref_time(2000.0);
-    int fp;
     int currel = 0;
     int freq[100];
     double duration[100];
@@ -27,12 +50,6 @@ int main()
         res = fscanf(stdin, "%s", command);
         ch = getc(stdin);
     } while (ch != EOF);
-//    printf("currel, %d\n", currel);
-//    printf("freq\n");
-//    print_array(freq, currel);
-//    printf("duration\n");
-//    print_darray(duration, currel);
-//    return 0;
     fp = open("/dev/tty0", O_WRONLY);
     play_sequence(fp, freq, duration, currel);
     close(fp);
